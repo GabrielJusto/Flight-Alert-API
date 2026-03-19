5a71cb81383e0f3a7cede29eb8b3d20f1a9f8690
Gabriel Bonatto Justo <gabriel.justo@acad.pucrs.br>
2026-03-19 11:50:32 -0300

    Add function to set notification as notified.

--- Controllers/RouteController.cs
+++ Controllers/RouteController.cs
@@ -12,11 +12,13 @@ namespace Flight_Alert_API.Controllers;
 [Route("/routes")]
 public class RouteController(
     IMonitoredRouteService monitoredRouteService,
-    ILogger<RouteController> logger
+    ILogger<RouteController> logger,
+    IWebHostEnvironment env
 ) : ControllerBase
 {
     private readonly IMonitoredRouteService _monitoredRouteService = monitoredRouteService;
     private readonly ILogger<RouteController> _logger = logger;
+    private readonly IWebHostEnvironment _env = env;
 
     [HttpPost("insert")]
     public async Task<IActionResult> InsertRoute([FromBody] RouteRegisterRequest request)
@@ -68,5 +70,25 @@ public class RouteController(
             _logger.LogError(ex, "An unexpected error occurred while getting all routes for user {UserId}", userId);
             return StatusCode(500, new { error = "An unexpected error occurred." });
         }
+    }
+
+    [HttpPost("process-notifications")]
+    public async Task<IActionResult> ProcessNotifications([FromServices] IFlightPriceService priceService)
+    {
+        if(!_env.IsDevelopment())
+            return NotFound();
+
+        await priceService.CheckAllFlightPricesAsync();
+        return Ok(new { message = "Rota disponível apenas em desenvolvimento." });
+    }
+
+    [HttpPost("send-alerts")]
+    public async Task<IActionResult> SendAlerts([FromServices] ISendAlertsService sendAlertsService)
+    {
+        if(!_env.IsDevelopment())
+            return NotFound();
+
+        await sendAlertsService.SendAlertsAsync();
+        return Ok(new { message = "Rota disponível apenas em desenvolvimento." });
     }
 }
